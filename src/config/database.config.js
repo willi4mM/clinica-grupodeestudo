1158e9a6ae040ce81467378b1101f9b3bb3fbf95
William Marreiro <williammarreirob@gmail.com>
2020-10-31 13:30:47 -0300

    s

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,1 +1,8 @@
 const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL,{
+    userNewUrlParser: true,
+    useVreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+})

99e4fad992b3699433416f66ee5ea79e2947e9db
William Marreiro <williammarreirob@gmail.com>
2020-10-31 13:50:17 -0300

    31.10

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URL,{
-    userNewUrlParser: true,
-    useVreateIndex: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
 })
--- src/server.js
+++ src/server.js
@@ -1,4 +1,12 @@
 const express = require('express');
-
+const app = require('./app');
 const dotenv = require('dotenv');
-c
+
+const connectDataBase = require('./config/database.config');
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+    PORT,
+    console.log(`Servidor rodando em modo ${process.env.NODE_ENV} na porta ${PORT}`)
+)
+
